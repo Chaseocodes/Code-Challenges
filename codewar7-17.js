0e1924ddf23a7c2313b328bee4cf837de22f0958
Chase O'Hara <stanleycohara@gmail.com>
2022-07-17 14:58:55 -0400

    codewar 7-17

---
+++ codewar7-17.js
@@ -0,0 +1,18 @@
+// CODEWAR 8 KYU CHALLENGE
+
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+
+// MY ANSWER:
+
+function invert(array) {
+    let addIn = array.map(num =>{
+      return num * -1
+    })
+    return addIn
+  }
