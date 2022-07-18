197e8e492ce6db43d5d4b10830afee3654034c78
Chase O'Hara <stanleycohara@gmail.com>
2022-07-18 15:02:28 -0400

    new chalenge

---
+++ codewar7-18_part2.js
@@ -0,0 +1,12 @@
+
+
+
+
+// MY ANSWER:
+function greet(name){
+    if(name === "Johnny"){
+      return "Hello, my love!";
+      }else if(name != "johnny"){
+        return "Hello, " + name + "!"
+      }
+  }
