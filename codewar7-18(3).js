8dc53e7982ccd0070f7bda1d66a037ee16d18f47
Chase O'Hara <stanleycohara@gmail.com>
2022-07-19 10:42:47 -0400

    challenge three

---
+++ codewar7-18(3).js
@@ -0,0 +1,16 @@
+//  7 KYU CODE CHALLENGE
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+//MY ANSWER:
+function descendingOrder(n){
+    let back = Number(n.toString().split('').sort(((a,b)=> b-a)).join(''))
+    return back
+   }
