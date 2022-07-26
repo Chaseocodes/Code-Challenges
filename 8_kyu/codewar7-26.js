611c7ad98b348981818b03e1ffa1b1e3f8642f05
Chase O'Hara <stanleycohara@gmail.com>
2022-07-26 14:00:32 -0400

    new challenge

---
+++ 8_kyu/codewar7-26.js
@@ -0,0 +1,16 @@
+// 8 kyu code challenge
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// My answer:
+
+function check(a, x) {
+    return a.includes(x)
+  }
+
+
+
