c05e70dbc16674a002ff4cb0c6408c07a8327d97
Chase O'Hara <stanleycohara@gmail.com>
2022-07-28 10:44:54 -0400

    new challenge

---
+++ 8_kyu/codewar7-28.js
@@ -0,0 +1,14 @@
+// 8 kyu codewar challenge
+
+// Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+
+// my answer
+
+function isDivideBy(number, a, b) {
+    if(number % a === 0 && number % b === 0){
+      return true
+    }else{
+      return false
+    }
+  }
