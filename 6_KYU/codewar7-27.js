e2ce06771f0ff5fef7a9d9e9512d89a06efbc075
Chase O'Hara <stanleycohara@gmail.com>
2022-07-27 20:01:22 -0400

    new challenge

---
+++ 6_KYU/codewar7-27.js
@@ -0,0 +1,17 @@
+// 6 kyu codewar
+
+
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+
+// my answer:
+
+
+function solution(string) {
+    return string.split(/(?=[A-Z])/).join(" ")
+}
