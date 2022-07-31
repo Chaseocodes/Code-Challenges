e6999a1339d14be963f243c25801b2ad4126d3bc
Chase O'Hara <stanleycohara@gmail.com>
2022-07-31 11:38:51 -0400

    new challenge

---
+++ 7_KYU/codewar7-31(2nd).js
@@ -0,0 +1,13 @@
+// 7 kyu code challenge
+
+
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+
+// my answer:
+
+
+function solution(str, ending){
+    return str.endsWith(ending)
+   }
