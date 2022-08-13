f6c6e4a0c6c79cf709459dcd4d9c2866501e6397
Chase O'Hara <stanleycohara@gmail.com>
2022-08-13 10:54:00 -0400

    new challenge

---
+++ 8_kyu/codewar8-13pt2.js
@@ -0,0 +1,12 @@
+// 8 kyu code challenge
+
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+
+
+// my answer:
+
+function reverseWords(str){
+    return str.split(" ").reverse().join(" ")
+  }
