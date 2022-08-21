138d4f4434c5bf3a910f0586ec4734ccbef8fe08
Chase O'Hara <stanleycohara@gmail.com>
2022-08-21 17:47:42 -0400

    new challenge

---
+++ 6_KYU/codewar8-21.js
@@ -0,0 +1,17 @@
+//  6 kyu code challenge 
+
+
+
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// my answer:
+function createPhoneNumber(numbers){
+    let format = "(xxx) xxx-xxxx";
+   
+   for(let i = 0; i < numbers.length; i++)
+   {
+     format = format.replace('x', numbers[i]);
+   }
+   
+   return format;
+ }
