5777b99d50c25b5d3e0956e16e92071b00ec594a
Chase O'Hara <stanleycohara@gmail.com>
2022-08-29 19:32:44 -0400

    new challenge

---
+++ 8_kyu/codewar8-29.js
@@ -0,0 +1,23 @@
+//  8 kyu code challenge
+
+
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+
+//  my answer:
+function defineSuit(card) {
+    let suit = card.substr(-1);
+    switch(suit){
+       case '♣': return 'clubs';
+       case '♦': return 'diamonds';
+       case '♥': return 'hearts';
+       default: return 'spades';
+    }
+ }
