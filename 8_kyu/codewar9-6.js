2e3a5cf1c44b3e7125bc8e740b583bbc1891bdd0
Chase O'Hara <stanleycohara@gmail.com>
2022-09-06 13:42:19 -0400

    new code challenge

---
+++ 8_kyu/codewar9-6.js
@@ -0,0 +1,16 @@
+// 8 kyu code war
+
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+
+
+// my answer:
+function apple(x){
+    let sqaure = x * x
+     if(sqaure >= 1000){
+       return "It's hotter than the sun!!"
+     }else{
+       return "Help yourself to a honeycomb Yorkie for the glovebox."
+     }
+   
+   }
