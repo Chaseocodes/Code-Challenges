251f4e36a8dab65d18d8bde76ab06fe947b53668
Chase O'Hara <stanleycohara@gmail.com>
2022-10-07 17:04:51 -0400

    new challenge

---
+++ 8_kyu/codewar10-2(2nd).js
@@ -0,0 +1,8 @@
+// 8 kyu code challenge
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+//  my answer:
+function nameShuffler(str){
+    return str.split(" ").reverse().join(" ")
+   }
