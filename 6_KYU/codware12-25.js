f29964496306bcbac35224fd74d1b32f86938568
Chase O'Hara <stanleycohara@gmail.com>
2023-12-25 11:39:05 -0500

    New challenge

---
+++ 6_KYU/codware12-25.js
@@ -0,0 +1,16 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+
+function findOutlier(integers){
+
+  let odd = integers.filter(num => num % 2 !== 0);
+  let even = integers.filter(num => num % 2 === 0);
+
+  if (odd.length === 1) {
+    return odd[0];
+  } else if (even.length === 1) {
+    return even[0];
+  }
+
+  return null;
+}
